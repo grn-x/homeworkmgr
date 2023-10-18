edec671aa0d38ffb3ca0c4ed7d84b1ccfe488c75
grn-x <benedikt.weingaertner@gmx.de>
2023-10-18 15:58:11 +0200

    instead of a nice mutliline jtable cell i added an ugly second textarea

--- homeworkmgr/src/de/grnx/homeworkmgr/main/HomeworkViewer.java
+++ homeworkmgr/src/de/grnx/homeworkmgr/main/HomeworkViewer.java
@@ -7,7 +7,10 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
 import org.w3c.dom.traversal.DocumentTraversal;
@@ -19,6 +22,8 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Vector;
@@ -35,6 +40,7 @@ public class HomeworkViewer extends JFrame {
         setLayout(new BorderLayout());
         //add(this,BorderLayout.CENTER);
         
+        
         JPanel p = new JPanel();
         p.setLayout(new GridLayout(1,2));
         
@@ -44,37 +50,26 @@ public class HomeworkViewer extends JFrame {
         p.add(saveButton);
         p.add(addColButton);
         add(p,BorderLayout.SOUTH);
+        
+        
+        JTextArea textArea = new JTextArea();//for the selection listener below
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        add(textArea, BorderLayout.NORTH);
 
         saveButton.addActionListener(new ActionListener() { 
       	  public void actionPerformed(ActionEvent e) {
-      		  ArrayList<EnSt> exList = new ArrayList<EnSt>();
-      		  for(int i =0; i<table.getRowCount();i++) {
-//      			  EnSt entry = new EnSt();
-//      			  entry.setStrSubject(getName());
-//      			  entry.setLocalDateTime();
-//      			  entry.setStrNote(getName());
-      			  
-      			  //System.out.println("\""+Main.isDateParsable(table.getValueAt(i, 1) != null ? table.getValueAt(i, 1).toString() : "")? (LocalDateTime) table.getValueAt(i, 1) : LocalDateTime.now() table.getValueAt(i, 2) != null ? table.getValueAt(i, 2).toString() : "")+"\"");
-      			  try {
-      			exList.add(new EnSt(
-      				    table.getValueAt(i, 0) != null ? table.getValueAt(i, 0).toString() : "",
-      				    		java.time.LocalDateTime.parse(table.getValueAt(i, 1)==null||table.getValueAt(i, 1).toString().isBlank()?LocalDateTime.now().format(Main.formatter).toString():table.getValueAt(i, 1).toString(), Main.formatter),
-      				    table.getValueAt(i, 2) != null ? table.getValueAt(i, 2).toString() : "",
-      				    table.getValueAt(i,3) != null?table.getValueAt(i, 3).toString():"Pending"
-      				));
-      			
-      				Popup.displayNotification(p, "Saved", 500);
-      			  }catch (Exception e1) {
-      				  	e1.printStackTrace();
-      				  	ErrView.showStackTraceErrorDialog(null, "Error when saving Homework probably because date parsing error. Leave time cell empty for todays date", e1);
-      			  }
-      		  }
-      		  
-  			//NoteHandler.saveNote(comboBox.getSelectedItem().toString(), now, textArea.getText());
-      		  Serializer.serializeObject(Settings.getDataLocation().getAbsolutePath(),(Object)exList);
+              saveHomeworkState(table);
 
-      		  
       	  }});
+        
+        
+
+
+        		  
+        	  
+        	
+        
         
         addColButton.addActionListener(new ActionListener() { 
         	  public void actionPerformed(ActionEvent e) { 
@@ -98,6 +93,94 @@ public class HomeworkViewer extends JFrame {
         statusColumn.setCellEditor(new DefaultCellEditor(comboBox));
 
 
+        
+        
+        
+        
+        // Custom cell renderer for the text area
+        TableCellRenderer tableCellRenderer = (table, value, isSelected, hasFocus, row, column) -> {
+            JTextArea area = new JTextArea();
+            area.setLineWrap(true);
+            area.setWrapStyleWord(true);
+            area.setText(value != null ? value.toString() : "");
+            area.setEditable(false);
+            return area;
+        };
+
+        table.getColumnModel().getColumn(2).setCellRenderer(tableCellRenderer);
+
+        // ListSelectionListener to update the text area based on the selected row
+        ListSelectionListener selectionListener = e -> {
+            if (!e.getValueIsAdjusting()) {
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow >= 0 && selectedRow < arrList.size()) {
+                    Object[] data = arrList.get(selectedRow);
+                    if (data.length > 2 && data[2] != null) {
+                        textArea.setText(data[2].toString());
+                    }
+                }
+            }
+        };
+
+        table.getSelectionModel().addListSelectionListener(selectionListener);
+    
+
+        
+        
+        
+        table.getColumnModel().getColumn(2).setCellRenderer(tableCellRenderer);
+        table.getSelectionModel().addListSelectionListener(selectionListener);
+
+        textArea.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // Handle key typed events here
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+                if ((e.isShiftDown() || e.isControlDown() || e.isAltDown() || e.isMetaDown()) && e.getKeyCode() == KeyEvent.VK_ENTER) {
+                	e.consume();
+
+                    // Add a new line in the text area
+                    textArea.append("\n");
+                } else if (!(e.isShiftDown() || e.isControlDown() || e.isAltDown() || e.isMetaDown()) && e.getKeyCode() == KeyEvent.VK_ENTER) {
+                	e.consume();
+                    // Save the value and put it back into the table at the current row and column index 3
+                    int selectedRow = table.getSelectedRow();
+                    if (selectedRow >= 0 && selectedRow < tableModel.getRowCount()) {
+                        tableModel.setValueAt(textArea.getText(), selectedRow, 2);
+                        
+                        textArea.setFocusable(false);
+                        textArea.setFocusable(false);
+                        new Thread(() -> {
+                            try {
+                                Thread.sleep(100);
+                                textArea.setFocusable(true);
+                            } catch (InterruptedException e1) {
+                                e1.printStackTrace();
+                            }
+                        }).start();
+                        
+          				//Popup.displayNotification(textArea, "Updated Homework Cell", 1000);
+
+                        saveHomeworkState(table);
+
+                    }
+                }
+             }
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+        });
+    
+        
+        
+        
+        
+        
+        
         JScrollPane scrollPane = new JScrollPane(table);
         add(scrollPane, BorderLayout.CENTER);
         setSize(600, 400);
@@ -105,19 +188,49 @@ public class HomeworkViewer extends JFrame {
         setVisible(true);
     }
 
-    public Object[] getRowData(int rowIndex) {
+    private Object[] getRowData(int rowIndex) {
         Vector<?> rowData = (Vector<?>) tableModel.getDataVector().elementAt(rowIndex);
         return rowData.toArray();
     }
 
-    public void updateTable(ArrayList<Object[]> arrList) {
+    private void updateTable(ArrayList<Object[]> arrList) {
         tableModel.setRowCount(0); // Clear existing data
         for (Object[] rowData : arrList) {
             tableModel.addRow(rowData);
         }
     }
 
-    public void addRow(Object[] rowData) {
+    private void addRow(Object[] rowData) {
         tableModel.addRow(rowData);
     }
+    
+    private void saveHomeworkState(JTable table) {
+		  ArrayList<EnSt> exList = new ArrayList<EnSt>();
+		  for(int i =0; i<table.getRowCount();i++) {
+//			  EnSt entry = new EnSt();
+//			  entry.setStrSubject(getName());
+//			  entry.setLocalDateTime();
+//			  entry.setStrNote(getName());
+			  
+			  //System.out.println("\""+Main.isDateParsable(table.getValueAt(i, 1) != null ? table.getValueAt(i, 1).toString() : "")? (LocalDateTime) table.getValueAt(i, 1) : LocalDateTime.now() table.getValueAt(i, 2) != null ? table.getValueAt(i, 2).toString() : "")+"\"");
+			  try {
+			exList.add(new EnSt(
+				    table.getValueAt(i, 0) != null ? table.getValueAt(i, 0).toString() : "",
+				    		java.time.LocalDateTime.parse(table.getValueAt(i, 1)==null||table.getValueAt(i, 1).toString().isBlank()?LocalDateTime.now().format(Main.formatter).toString():table.getValueAt(i, 1).toString(), Main.formatter),
+				    table.getValueAt(i, 2) != null ? table.getValueAt(i, 2).toString() : "",
+				    table.getValueAt(i,3) != null?table.getValueAt(i, 3).toString():"Pending"
+				));
+			
+				Popup.displayNotification(table, "Saved", 1000);
+			  }catch (Exception e1) {
+				  	e1.printStackTrace();
+				  	ErrView.showStackTraceErrorDialog(null, "Error when saving Homework probably because date parsing error. Leave time cell empty for todays date", e1);
+			  }
+		  }
+		  
+		//NoteHandler.saveNote(comboBox.getSelectedItem().toString(), now, textArea.getText());
+		  Serializer.serializeObject(Settings.getDataLocation().getAbsolutePath(),(Object)exList);
+
+    }
+    
 }
